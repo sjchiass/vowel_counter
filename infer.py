b6df664debedb04c83eae265e670908ab6e26994
Sebastien Chiasson <sjchiass@gmail.com>
2022-10-16 10:21:40 -0400

    Let infer test multiple words

--- infer.py
+++ infer.py
@@ -1,21 +1,33 @@
 import tensorflow as tf
 from tensorflow import keras
-from PIL import Image, ImageDraw, ImageFont
+import numpy as np
 import argparse
+from vowelize import vowelize
 from make_image import make_image
 
 parser = argparse.ArgumentParser(
     description="Guess the amount of vowels in a string (letter y is not well supported)")
 parser.add_argument("model", type=str, help="Path of saved model")
-parser.add_argument("word", type=str, help="A word to try to guess")
+parser.add_argument("words", type=str, nargs='+', help="Words to try to guess")
 
 args = parser.parse_args()
 
 model = keras.models.load_model(args.model)
 
-img_array = make_image(args.word, 0, as_array=True)
-img_array = tf.expand_dims(img_array, 0)  # Create batch axis
+data = []
+for word in args.words:
+    img_array = make_image(word, 0, as_array=True)
+    img_array = tf.expand_dims(img_array, 0)  # Create batch axis
+    data.append(img_array)
 
-predictions = model.predict(img_array)
-for n, i in enumerate(predictions.flatten()):
-    print(f"{n} vowels: {i:5.2f}")
+data = np.concatenate(data, axis=0)
+predictions = model.predict(data)
+
+for o, w in zip(range(predictions.shape[0]), args.words):
+    obs = predictions[o,:]
+    print(f"*** {w.upper():10} has {vowelize(w):2} vowels ***")
+    guess = np.argmax(obs.flatten())
+    print(f"--> Best guess is  {guess:2} {'PASS' if guess == vowelize(w) else 'FAIL'}   <--")
+    for n, i in enumerate(obs.flatten()):
+        if f"{i:.2%}" != "0.00%":
+            print(f"\-> % certain that '{w.lower()}' has {n} vowels: {i:6.2%}")
